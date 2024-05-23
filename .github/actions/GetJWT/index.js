7881a60bc35add9834fe6cf43b3b4acc3432d505
Dylan <dylan.langston@gmail.com>
2024-05-23 21:00:29 +0000

    Alternative method to retrieve JWT

--- .github/actions/GetJWT/index.js
+++
@@ -1,6 +0,0 @@
-const core = require('@actions/core');
-async function getIDTokenAction() {
-   const token = await core.getIDToken();
-   core.setOutput("token", token);
-}
-await getIDTokenAction();
