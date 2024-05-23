e8dcd8c96aa585529b2f5505e3c0d74836f0b214
Dylan Langston <mail@dylanlangston.com>
2024-05-23 01:24:05 +0000

    Fix flaky background image on webkit

--- src/tests/integration.spec.ts
+++ src/tests/integration.spec.ts
@@ -1,8 +1,9 @@
 import { test, expect } from '@playwright/test';
 
 test('Ensure layout is correct', async ({ page }) => {
-  await page.goto('');
-  await page.reload();
+  await page.goto('', {
+    waitUntil: "load"
+  });
 
   await expect(page).toHaveScreenshot("body.png", {
     clip: {
