e6d4c32ef1e344be705dd6d76fbfb8b83c136285
Dylan <dylan.langston@gmail.com>
2024-05-24 11:31:38 +0000

    Attempt to work around github action limitations

--- src/tests/reporters/playwright-github-actions-reporter.ts
+++ src/tests/reporters/playwright-github-actions-reporter.ts
@@ -1,3 +1,4 @@
+import * as fs from 'fs/promises';
 import * as path from 'path';
 import { DefaultArtifactClient } from '@actions/artifact';
 import Summary from './github-actions-summary';
@@ -7,9 +8,11 @@ import type * as reporterTypes from 'playwright/types/testReporter';
 const artifactClient = new DefaultArtifactClient();
 const [owner, repo] = (process.env.GITHUB_REPOSITORY || '').split('/');
 async function uploadArtifact(filePath: string, browser: string, testName: string, attempt: number): Promise<string> {
-  const fileName = `${testName}-${browser}-${attempt}-${path.basename(filePath)}`;
+  const fileName = `${testName}-${browser}-${attempt}-${path.basename(filePath)}.zip`;
   try {
-    const uploadResponse = await artifactClient.uploadArtifact(fileName, [filePath], '/', {
+    const zipName = `${fileName}.zip`;
+    await fs.copyFile(fileName, zipName)
+    const uploadResponse = await artifactClient.uploadArtifact(fileName, [zipName], '/', {
       compressionLevel: 0
     });
     const runId = process.env.GITHUB_RUN_ID;
@@ -24,42 +27,44 @@ async function uploadArtifact(filePath: string, browser: string, testName: strin
 class PlaywrightGitHubActionsReporter implements reporterTypes.Reporter {
   private summary = new Summary();
 
-  async onTestEnd(test: reporterTypes.TestCase, result: reporterTypes.TestResult): Promise<void> {
+  onTestEnd(test: reporterTypes.TestCase, result: reporterTypes.TestResult): void {
     const testName = test.title;
     const status = result.status === 'passed' ? 'success' : 'failure';
     const browser = test.parent.project()!.name;
     const summaryTitle = `🎭 ${testName} ${browser} test result: ${status} (Attempt #${test.retries + 1})`;
     const duration = `Duration: ${result.duration}ms`;
 
-    if (result.status === 'failed') {
-      const error = result.error?.message?.replace(/[\u0000-\u001F\u007F-\u009F]/g, "");
-      this.summary.addHeading(summaryTitle, 4);
-      this.summary.addRaw(`${duration}`, true);
-      this.summary.addQuote(error!);
-      const attachments = result.attachments || [];
-      const snapshotFiles = attachments.filter((a: any) => a.name.toLowerCase().endsWith(".png"));
-      const actualImage = snapshotFiles.find(s => s.name.endsWith("-actual.png"));
-      const diffImage = snapshotFiles.find(s => s.name.endsWith("-diff.png"));
-      const expectedImage = snapshotFiles.find(s => s.name.endsWith("-expected.png"));
+    setTimeout(async () => {
+      if (result.status === 'failed') {
+        const error = result.error?.message;
+        this.summary.addHeading(summaryTitle, 4);
+        this.summary.addRaw(`${duration}`, true);
+        this.summary.addQuote(error!);
+        const attachments = result.attachments || [];
+        const snapshotFiles = attachments.filter((a: any) => a.name.toLowerCase().endsWith(".png"));
+        const actualImage = snapshotFiles.find(s => s.name.endsWith("-actual.png"));
+        const diffImage = snapshotFiles.find(s => s.name.endsWith("-diff.png"));
+        const expectedImage = snapshotFiles.find(s => s.name.endsWith("-expected.png"));
 
-      if (actualImage && diffImage && expectedImage) {
-        const [actualURL, diffURL, expectedURL] = await Promise.all([
-          uploadArtifact(actualImage.path!, browser, testName, test.retries),
-          uploadArtifact(diffImage.path!, browser, testName, test.retries),
-          uploadArtifact(expectedImage.path!, browser, testName, test.retries)
-        ]);
+        if (actualImage && diffImage && expectedImage) {
+          const [actualURL, diffURL, expectedURL] = await Promise.all([
+            uploadArtifact(actualImage.path!, browser, testName, test.retries),
+            uploadArtifact(diffImage.path!, browser, testName, test.retries),
+            uploadArtifact(expectedImage.path!, browser, testName, test.retries)
+          ]);
 
-        this.summary.addEOL();
-        this.summary.addRaw('| Original | Diff | Actual |', true);
-        this.summary.addRaw('|---|---|---|', true);
-        this.summary.addRaw(`| ![Original](${actualURL}) | ![Diff](${diffURL}) | ![Actual](${expectedURL}) |`, true);
+          this.summary.addEOL();
+          this.summary.addRaw('| Original | Diff | Actual |', true);
+          this.summary.addRaw('|---|---|---|', true);
+          this.summary.addRaw(`| ![Original](${actualURL}) | ![Diff](${diffURL}) | ![Actual](${expectedURL}) |`, true);
+        }
+      } else {
+        this.summary.addHeading(summaryTitle, 4);
+        this.summary.addRaw(`${duration}\n`);
       }
-    } else {
-      this.summary.addHeading(summaryTitle, 4);
-      this.summary.addRaw(`${duration}\n`);
-    }
 
-    await this.summary.write({ overwrite: false });
+      await this.summary.write({ overwrite: false });
+    });
   }
 }
 
